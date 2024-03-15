a156353008a6fdc30e3993bb531bc710c7a1ce6a
Sophfaraway <molengrafova121@trebesin.cz>
2024-03-15 13:10:22 +0100

    .

---
+++ PRG/python vs javascript/class/oop.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self, name, school, color):
+        self.name = name
+        self.school = school
+        self.fav_color = color
+
+    def speak(self, greeting):
+        print(f"{greeting}My favourite color is {self.fav_color}")
+
+
+class Zak(Student):
+    def __init__(self, name, school, color):
+        super().__init__(self, name, school, color)
+
+
+hvezdon = Student("Hvězdoň", "Třebešín", "yellow")
+
+print(hvezdon.fav_color)
+hvezdon.speak("Hewwo ")
+
+rizek = Student("Řízek", "Úžlabina", "blue")
+rizek.speak("Od slova řízek ")
