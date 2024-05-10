ea092deca1ac293e483b33f9eb0352558093a346
Sophfaraway <molengrafova121@trebesin.cz>
2024-05-10 13:07:35 +0200

    45

--- PRG/python vs javascript/hra/items.py
+++ PRG/python vs javascript/hra/items.py
@@ -3,7 +3,7 @@ import pygame
 # from pygame.sprite import _Group
 
 
-class Desk(pygame.sprite.Sprite):
+class Item(pygame.sprite.Sprite):
     def __init__(self, image, width, height, position):
         super().__init__()
         self.image = image
@@ -11,41 +11,21 @@ class Desk(pygame.sprite.Sprite):
         self.height = height
         self.position = position
         self.scale = 1
-        self.scale_desk()
+        self.scale_item()
         self.rect = self.image.get_rect(topleft=self.position)
 
-    def scale_desk(self):
+    def scale_item(self):
         scaled_size = (self.width * self.scale, self.height * self.scale)
         self.image = pygame.transform.scale(self.image, scaled_size)
 
 
-class Coin(pygame.sprite.Sprite):
-    def __init__(self, image, width, height, position):
-        super().__init__()
-        self.image = image
-        self.width = width
-        self.height = height
-        self.position = position
-        self.scale = 1
-        self.scale_coin()
-        self.rect = self.image.get_rect(topleft=self.position)
-
-    def scale_coin(self):
-        scaled_size = (self.width * self.scale, self.height * self.scale)
-        self.image = pygame.transform.scale(self.image, scaled_size)
+class Coin(Item):
+    pass
 
 
-class PowerUp(pygame.sprite.Sprite):
-    def __init__(self, image, width, height, position):
-        super().__init__()
-        self.image = image
-        self.width = width
-        self.height = height
-        self.position = position
-        self.scale = 1
-        self.scale_pu()
-        self.rect = self.image.get_rect(topleft=self.position)
+class Desk(Item):
+    pass
 
-    def scale_pu(self):
-        scaled_size = (self.width * self.scale, self.height * self.scale)
-        self.image = pygame.transform.scale(self.image, scaled_size)
+
+class PowerUp(Item):
+    pass
